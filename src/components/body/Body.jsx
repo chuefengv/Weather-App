e56323e8d9302120f00824513237f519bf3e4fa2
chuefeng <feng.vang98@gmail.com>
2021-02-10 18:06:14 -0500

    set up body grid, and the display

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
+import Display from './display'
 import './Body.css'
 
 function Body(){
     return(
         <div className='body'>
             Body
+            <Display className='display' />
         </div>
     )
 }
