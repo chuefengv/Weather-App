e8422d76bf830c3822ca9d48918d45a59fdcd68b
chuefeng <feng.vang98@gmail.com>
2021-02-10 18:16:20 -0500

    set up foot grid, and set up the searchbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Searchbar from './searchbar'
 import './Footer.css'
 
 function Footer(){
     return(
         <div className='footer'>
             Footer
+            <Searchbar className='searchbar' />
         </div>
     )
 }
---
+++ src/components/footer/searchbar/Searchbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Searchbar.css'
+
+function Searchbar(){
+    return(
+        <div className='searchbar'>
+            Searchbar
+        </div>
+    );
+};
+
+export default Searchbar;
