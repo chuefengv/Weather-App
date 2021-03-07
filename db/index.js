3414d1d16bbe48b1e75f7a5127d841f9518ea578
chuefengv <feng.vang98@gmail.com>
2021-03-07 16:52:37 -0500

    testing server db

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    password: (process.env.DB_PASS || '46a15438110934967ab8afa2c346428dfcaa26a40b3cf0abc13ccdf748d7141b'),
-    user: (process.env.DB_USER || 'vsrvwhlczuhayc'),
-    database: (process.env.DB || 'd617u788n94tn'),
-    host: (process.env.DB_HOST || 'ec2-52-71-231-37.compute-1.amazonaws.com'),
+    password: (process.env.DB_PASS),
+    user: (process.env.DB_USER ),
+    database: (process.env.DB),
+    host: (process.env.DB_HOST),
     port: '5432',
     ssl: {rejectUnauthorized:false}
 });
