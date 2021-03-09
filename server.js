03f8a10925f626cfacea4f921daa1c860baab5a1
chuefengv <feng.vang98@gmail.com>
2021-03-08 22:20:18 -0500

    test

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const pool = require('./db/index')
 
 app.use(cors()); 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://cv-weather-radar.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
 
-app.get('/api/data?state=', async(req, res)=>{
+app.get('/api/data?', async(req, res)=>{
     const state = req.query.state;
     try{
         const getCity = await pool.query("SELECT city, id FROM uscities WHERE state_name=($1) ORDER BY city",[state]);
