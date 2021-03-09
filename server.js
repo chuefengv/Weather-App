482735b8c4a59f3e9e90e76e477bff138b7d510d
chuefengv <feng.vang98@gmail.com>
2021-03-08 23:59:06 -0500

    test

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const pool = require('./db/index')
 
 app.use(cors()); 
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTION");
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); 
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTION");
 
-    next();
-  });
+//     next();
+//   });
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
 
-app.get('/api/data?', async(req, res)=>{
+app.get('/api/data', async(req, res)=>{
     const state = req.query.state;
     try{
         const getCity = await pool.query("SELECT city, id FROM uscities WHERE state_name=($1) ORDER BY city",[state]);
